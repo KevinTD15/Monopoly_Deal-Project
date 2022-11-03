705e290a3e5918d85827e5c4700b6b218453edbd
KevinTD15 <talaveradiazkevin1999@gmail.com>
2022-11-03 04:01:52 -0500

    diccionario para tablero

--- jugador.py
+++ jugador.py
@@ -2,8 +2,26 @@ from abc import ABC, abstractstaticmethod
 
 class Jugador(ABC):
 
-    def __init__(self, nombre):
+    mano = []
+    tablero = {
+        'carmelita' : [],
+        'azulClaro' : [],
+        'morado' : [],
+        'anaranjado' : [],
+        'rojo' : [],
+        'amarillo' : [],
+        'verde' : [],
+        'azul' : [],
+        'negro' : [],
+        'blanco' : [],
+        'dinero' : []
+    }
+    
+    def __init__(self, nombre, esBot):
         self.nombre = nombre
+        self.esBot = esBot
+        self.mano = []
+        self.tablero = {}
         
     @abstractstaticmethod
     def SeleccionarJugada(posiblesJugadas):
@@ -11,6 +29,12 @@ class Jugador(ABC):
     
     def Jugar(posiblesJugadas):
         return Jugador.SeleccionarJugada(posiblesJugadas)
+    
+    #def PonerMano(self, carta):
+    #    self.mano.append(carta)
+    
+    def PonerEnTablero():
+        pass
     
     
     
