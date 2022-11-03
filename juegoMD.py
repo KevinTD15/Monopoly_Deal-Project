91347a52ec4820ba8ee76cd2bd4032b65f206f80
KevinTD15 <talaveradiazkevin1999@gmail.com>
2022-11-02 22:32:12 -0500

    clase juego monopoly creada (implementar metodos)

---
+++ juegoMD.py
@@ -0,0 +1,42 @@
+from juego import Juego
+from cartasMD import *
+
+class JuegoMD(Juego):
+
+    def BuscarGanador():
+        #put your code here
+        pass
+    
+    def EjecutarJugada():
+        #put your code here
+        pass
+    
+    def FinDeJuego():
+        #put your code here
+        pass
+    
+    def OrganizarJugadas():
+        #put your code here
+        pass
+    
+    def PosiblesJugadas():
+        #put your code here
+        pass
+    
+    def RepartirCartas():
+        #put your code here
+        pass
+
+    def IndiceGanador():
+        pass
+
+        
+#test
+j = JuegoMD()
+j.Final = True
+j.IndiceGanador = 1
+print(j.IndiceGanador)
+        
+    
+
+    
