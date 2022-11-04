8b9a4e8ef9e0fe4df69e763b83b45fc5ff2d546d
KevinTD15 <talaveradiazkevin1999@gmail.com>
2022-11-03 21:32:54 -0500

    UsarCarta y FinDeJuego implementado

--- juegoMD.py
+++ juegoMD.py
@@ -1,16 +1,16 @@
 from cartasMD import *
 from mazoCartas import *
-from jugador import *
+from jugadorAleatorio import *
 from jugada import *
 import time
 import random as rd
 
 class JuegoMD:
 
-    _jugadores = [Jugador]
-    _jugadas = [Jugada]
+    _jugadores = []
+    _jugadas = []
     _indiceJugadorActual = 0
-    _manoJugador = [object]
+    _manoJugador = []
     _iniciarJuegoMD = True
     _mazo = Mazo.cartas
     descarte = []
@@ -26,9 +26,12 @@ class JuegoMD:
         #put your code here
         pass
     
-    def FinDeJuegoMD():
-        #put your code here
-        pass
+    def FinDeJuegoMD(): #no se si esto pinche
+        count = 0
+        for i in JuegoMD._jugadores[JuegoMD._indiceJugadorActual].tablero:
+            if(len(i) >= 1 and i[0].tipo == 'propiedad' and len(i) == i.cantGrupo):
+                count += 1
+        return count == 3
     
     def OrganizarJugadas():
         #put your code here
@@ -64,6 +67,14 @@ class JuegoMD:
             raise Exception('El tiene que haber un ganador siempre')
         else:
             print(f'Gana el jugador {JuegoMD._jugadores[JuegoMD.indiceGanador]}')
+            
+    def UsarCarta(carta):        
+        if(carta.tipo == 'propiedad'):
+            JuegoMD._jugadores[JuegoMD._indiceJugadorActual].tablero[carta.color] = carta
+        elif(carta.tipo == 'dinero'):
+            JuegoMD._jugadores[JuegoMD._indiceJugadorActual].tablero[carta.tipo] = carta
+        else:
+            pass
             
     def EjecutarJuegoMD():
         '''aqui es donde se lleva a cabo la ejecucion de todo el JuegoMD'''
@@ -103,6 +114,10 @@ class JuegoMD:
         
 #test
 j = JuegoMD()
+j1 = JugadorAleatorio('pepe', True)
+j2 = JugadorAleatorio('jose', True)
+j._jugadores.append(j1)
+j._jugadores.append(j2)
 j.Simulador()
         
     
