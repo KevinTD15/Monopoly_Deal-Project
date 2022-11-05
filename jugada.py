047fb49dcdfff1f98cfd79cb54fa40ae7ca4af29
KevinTD15 <talaveradiazkevin1999@gmail.com>
2022-11-05 15:38:38 -0500

    implementada la jugada random

--- jugada.py
+++ jugada.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractclassmethod
 from jugador import *
 import random as rd
+import juegoMD
 
 class Jugada(ABC):
     def __init__(self, jugador, mazo, descarte) -> None:
@@ -12,22 +13,72 @@ class Jugada(ABC):
     def CrearJugada():
         pass
     
-    def EvaluarJugada(cartasAJugar): #Heuristica
+    @abstractclassmethod
+    def DescartarCartas():
         pass
-
+    
+    @abstractclassmethod
+    def UsarCarta():
+        pass
+    
 class JugadaRandom(Jugada):
     def __init__(self, jugador, mazo, descarte) -> None:
         super().__init__(jugador, mazo, descarte)
     
     def CrearJugada(self):
-        cantCartasAJugar = min(rd.randint(0, 3), len(self.jugador.mano))
-        cartasAJugar = []
-
-        for i in range(cantCartasAJugar):
-            cartasAJugar.append(self.jugador.mano[rd.randint(0, len(self.jugador.mano))])
+        a = rd.randint(0, 3)
+        cantCartasAJugar = min(a, len(self.jugador.mano))
+        cartasAJugar = rd.sample(self.jugador.mano, cantCartasAJugar)
                 
-        eval = self.EvaluarJugada(cartasAJugar)
-        
-        return cartasAJugar, eval
+        return cartasAJugar
+    
+    def DescartarCartas(self): #ver xq descarto propiedades
+        cantCartasADescartar = len(self.jugador.mano) - 7
+        indicesUsados = []
+        for i in range(cantCartasADescartar):
+            a = rd.randint(0, len(self.jugador.mano) - 1)
+            if(a in indicesUsados or self.jugador.mano[a].tipo == 'propiedad'):
+                i -= 1
+            else:
+                juegoMD.JuegoMD.notificaciones.append(f'{self.jugador.nombre} descarta {self.jugador.mano[a].tipo} {self.jugador.mano[a].nombre}') 
+                indicesUsados.append(a)
+                self.descarte.append(self.jugador.mano.pop(a))
+    
+    def UsarCarta(self, carta):
+        jugadorActual = self.jugador
+        juegoMD.JuegoMD.notificaciones.append(f'{jugadorActual.nombre} intentara jugar la {carta.tipo} {carta.nombre}') 
+        if(carta.tipo == 'propiedad'):
+            jugadorActual.tablero[carta.color].append(carta)
+            jugadorActual.mano.remove(carta)
+        elif(carta.tipo == 'dinero'):
+            jugadorActual.tablero[carta.tipo].append(carta)
+            jugadorActual.mano.remove(carta)
+        elif(carta.tipo == 'comodin'):
+            if (len(carta.color) > 0):
+                col = rd.sample(carta.color, 1)
+                if (col[0] in jugadorActual.tablero and len(jugadorActual.tablero[col[0]]) > 0 and len(jugadorActual.tablero[col[0]]) < 3):
+                    jugadorActual.tablero[col[0]].append(carta)
+                else:
+                    jugadorActual.tablero['dinero'].append(carta)
+            else:
+                col = rd.sample(sorted(jugadorActual.tablero), 1)
+                if (col[0] in jugadorActual.tablero and len(jugadorActual.tablero[col[0]]) > 0 and len(jugadorActual.tablero[col[0]]) < 3):
+                    jugadorActual.tabblero[col[0]].append(carta)
+                else:
+                    pass               
+            jugadorActual.mano.remove(carta)
+        else:
+            if(carta.subtipo == 'renta'):
+                pass
+            elif(carta.subtipo == 'construccion'):
+                pass
+            elif(carta.subtipo == 'rapida'):
+                pass
+            elif(carta.subtipo == 'robarprop'):
+                pass
+            elif(carta.subtipo == 'robardinero'):
+                pass
+            else: #robar carta
+                pass
             
     
