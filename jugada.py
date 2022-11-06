908723c65c44f063fba9eab771080815ba03f37b
KevinTD15 <talaveradiazkevin1999@gmail.com>
2022-11-05 21:31:43 -0500

    creada estructura para que otros jugadores puedan responder

--- jugada.py
+++ jugada.py
@@ -5,10 +5,11 @@ import juegoMD
 from crupier import *
 
 class Jugada(ABC):
-    def __init__(self, jugador, mazo, descarte) -> None:
+    def __init__(self, jugador, mazo, descarte, jugadores = None):
         self.jugador = jugador
         self.mazo = mazo
         self.descarte = descarte
+        self.jugadores = jugadores
     
     @abstractclassmethod
     def CrearJugada():
@@ -22,9 +23,13 @@ class Jugada(ABC):
     def UsarCarta():
         pass
     
+    @abstractclassmethod
+    def ResponderAJugada():
+        pass
+    
 class JugadaRandom(Jugada):
-    def __init__(self, jugador, mazo, descarte) -> None:
-        super().__init__(jugador, mazo, descarte)
+    def __init__(self, jugador, mazo, descarte, jugada = None):
+        super().__init__(jugador, mazo, descarte, jugada)
     
     def CrearJugada(self):
         a = rd.randint(0, 3)
@@ -44,6 +49,9 @@ class JugadaRandom(Jugada):
                 juegoMD.JuegoMD.notificaciones.append(f'{self.jugador.nombre} descarta {self.jugador.mano[a].tipo} {self.jugador.mano[a].nombre}') 
                 indicesUsados.append(a)
                 self.descarte.append(self.jugador.mano.pop(a))
+                
+    def ResponderAJugada(self, jugadorActual, carta):
+        pass
     
     def UsarCarta(self, carta):
         jugadorActual = self.jugador
@@ -79,14 +87,38 @@ class JugadaRandom(Jugada):
         else:
             
             if(carta.subtipo == 'renta'):
-                pass
+                posiblePago = []
+                for i in jugadorActual.tablero:
+                    if(len(jugadorActual.tablero[i]) > 0 and len(carta.propiedades) == 0 and i != 'dinero'):
+                        posiblePago.append(i)
+                    elif(len(jugadorActual.tablero[i]) > 0 and i in carta.propiedades):
+                        posiblePago.append(i)
+                if(len(posiblePago) == 0):
+                    jugadorActual.tablero['dinero'].append(carta)
+                    juegoMD.JuegoMD.notificaciones.append(f'{jugadorActual.nombre} la carta {carta.tipo} {carta.nombre} como dinero')
+                else:
+                    col = rd.sample(posiblePago, 1)
+                    if(carta.todos):
+                        for j in self.jugadores:
+                            if (j != jugadorActual):
+                                j.Responder(jugadorActual, self.mazo, self.descarte, carta)
+                    else:
+                        jugs = self.jugadores.copy()
+                        jugs.remove(jugadorActual)
+                        jug = rd.sample(jugs, 1)
+                        jug[0].Responder(jugadorActual, self.mazo, self.descarte, carta)
+                        pass
+                self.descarte.append(carta)
+                jugadorActual.mano.remove(carta)
             
             elif(carta.subtipo == 'construccion'):
-                col = rd.sample(sorted(jugadorActual.tablero), 1)
-                if(carta.nombre == 'casa' and col != 'dinero'):
+                coloresTablero = sorted(jugadorActual.tablero).copy()
+                coloresTablero.remove('dinero')
+                col = rd.sample(coloresTablero, 1)
+                if(carta.nombre == 'casa'):
                     if (len(jugadorActual.tablero[col[0]]) > 0 and len(jugadorActual.tablero[col[0]]) == jugadorActual.tablero[col[0]][0].cantGrupo):
                         jugadorActual.tablero[col[0]].append(carta)
-                elif(carta.nombre == 'hotel' and col != 'dinero'):
+                elif(carta.nombre == 'hotel'):
                     if (len(jugadorActual.tablero[col[0]]) > 0 and len(jugadorActual.tablero[col[0]]) == jugadorActual.tablero[col[0]][0].cantGrupo + 1):
                             jugadorActual.tablero[col[0]].append(carta)
                 else:
@@ -105,5 +137,4 @@ class JugadaRandom(Jugada):
             else: #robar carta
                 Crupier.RepartirCartas(False, jugadorActual, self.mazo, int(carta.cantCartasATomar), self.descarte)
                 juegoMD.JuegoMD.notificaciones.append(f'{jugadorActual.nombre} usa la {carta.tipo} {carta.nombre} y toma {carta.cantCartasATomar} del mazo')                
-                pass
     
