4d91e68c277d0166185958578b933236f3419bf7
KevinTD15 <talaveradiazkevin1999@gmail.com>
2022-11-10 01:30:36 -0500

    implementando ranking para evaluador

--- utiles.py
+++ utiles.py
@@ -70,4 +70,16 @@ def CantidadDeConstrucciones(listaDeUnColor):
         if(i.tipo == 'accion'):
             cont += 1
     return cont
+
+def RankingPropiedades(jugadorActual):
+    rank = {}
+    for i in jugadorActual.tablero:
+        if(len(jugadorActual.tablero[i]) > 0 and CantidadDeConstrucciones(jugadorActual.tablero[i]) == 0):
+            val = jugadorActual.tablero[i][0].cantGrupo - len(jugadorActual.tablero[i])
+            rank[val] = i
+        elif(len(jugadorActual.tablero[i]) == 0):
+            pass
+            #HACER ALGOOOOOOOOOOOOOO
+    return rank
+    
         
