8e8120efa62432c4e7d6925afec1b1eb94b087e9
KevinTD15 <talaveradiazkevin1999@gmail.com>
2023-01-01 09:46:09 -0500

    clases representativas para el juego

---
+++ dsl/juego.py
@@ -0,0 +1,28 @@
+class CrearJuego:
+    '''
+        clase que representa la cracion del juego
+    '''
+    
+class CrearJugador:
+    '''
+        Esta clase representa la creacion del jugador.
+        La instrucción imprimir únicamente tiene como parámetro una cadena
+    '''
+
+    def __init__(self, tipo,id) :
+        self.tipo = tipo
+        self.id=id
+
+class ComenzarJuego:
+    '''
+        clase que representa el comienzo del juego
+    '''
+
+class IdFuncionJuego() :
+    '''
+        Esta clase representa el id de las funciones del juego.
+        La instrucción mientras recibe como parámetro una expresión lógica y la lista
+        de instrucciones a ejecutar si la expresión lógica es verdadera.
+    '''
+    def __init__(self, id) :
+        self.id=id
