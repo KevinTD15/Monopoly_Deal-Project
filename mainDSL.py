f2eed02594f6abb4df74b71edf47247963400fab
KevinTD15 <talaveradiazkevin1999@gmail.com>
2023-01-02 01:54:26 -0500

    arreglado para que sea mas sencillo cargar .mpd

--- mainDSL.py
+++ mainDSL.py
@@ -1,8 +1,22 @@
 from dsl.interprete import inicia
+import os
+
 def main():
-    f = open("dsl/Ejemplos/juegoMD.mpd", "r")
-    input = f.read()
-    inicia(input)
+    print('Teclee direccion de archivo \'.mpd\'')
+    pathInput = input()
+    if(pathInput != None):
+        path = pathInput
+    else:
+        path = "dsl/Ejemplos/juegoMD.mpd"
+        
+    if not os.path.isfile(path) :
+        print('Archivo no existe')
+    elif not path.endswith('.mpd'):
+        print('Archivo no es \'mpd\'')
+    else:
+        f = open(path, "r")
+        texto = f.read()
+        inicia(texto)
     
 if __name__ == '__main__':
     main()
