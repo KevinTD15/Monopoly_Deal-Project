f5495446d940531e37c76e8abcb7dfa8e1726ff1
KevinTD15 <talaveradiazkevin1999@gmail.com>
2023-01-02 01:04:05 -0500

    se organizo el py de instrucciones

--- dsl/instrucciones.py
+++ dsl/instrucciones.py
@@ -1,66 +1,48 @@
-class Instruccion:
-    '''This is an abstract class'''
-
-class Imprimir(Instruccion) :
+class Imprimir:
     '''
         Esta clase representa la instrucción imprimir.
-        La instrucción imprimir únicamente tiene como parámetro una cadena
+        Recibe como parámetro una cadena
     '''
-
-    def __init__(self,  cad) :
+    def __init__(self, fila, cad ) :
+        self.fila = fila
         self.cad = cad
 
-class Len(Instruccion) :
-    '''
-        Esta clase representa la instrucción imprimir.
-        La instrucción imprimir únicamente tiene como parámetro una cadena
-    '''
-
-    def __init__(self,  cad) :
-        self.cad = cad
-
-class While(Instruccion) :
+class While :
     '''
         Esta clase representa la instrucción mientras.
         La instrucción mientras recibe como parámetro una expresión lógica y la lista
         de instrucciones a ejecutar si la expresión lógica es verdadera.
     '''
 
-    def __init__(self, expLogica, instrucciones = []) :
+    def __init__(self, fila, expLogica, instrucciones = []) :
+        self.fila = fila
         self.expLogica = expLogica
         self.instrucciones = instrucciones
 
-class Asignacion(Instruccion) :
+class Asignacion:
     '''
         Esta clase representa la instrucción de asignación de variables
         Recibe como parámetro el identificador a asignar y el valor que será asignado.
     '''
 
-    def __init__(self, id, exp) :
+    def __init__(self, fila, id,  exp) :
+        self.fila = fila
         self.id = id
         self.exp = exp
 
-class AgregarElemLista(Instruccion) :
-    '''
-        Esta clase instancia nombre lista y el id a anadir.
-        Recibe como parámetro el valor del token procesado por el analizador léxico
-    '''
-    def __init__(self, id, exp) :
-        self.id=id
-        self.exp = exp
-
-class If(Instruccion) : 
+class If : 
     '''
         Esta clase representa la instrucción if.
         La instrucción if recibe como parámetro una expresión lógica y la lista
         de instrucciones a ejecutar si la expresión lógica es verdadera.
     '''
 
-    def __init__(self, expLogica, instrucciones = []) :
+    def __init__(self, fila, expLogica, instrucciones = []) :
+        self.fila = fila
         self.expLogica = expLogica
         self.instrucciones = instrucciones
 
-class IfElse(Instruccion) : 
+class IfElse : 
     '''
         Esta clase representa la instrucción if-else.
         La instrucción if-else recibe como parámetro una expresión lógica y la lista
@@ -68,41 +50,62 @@ class IfElse(Instruccion) :
         a ejecutar si la expresión lógica es falsa.
     '''
 
-    def __init__(self, expLogica, instrIfVerdadero = [], instrIfFalso = []) :
+    def __init__(self, fila, expLogica, instrIfVerdadero = [], instrIfFalso = []) :
+        self.fila = fila
         self.expLogica = expLogica
         self.instrIfVerdadero = instrIfVerdadero
         self.instrIfFalso = instrIfFalso
 
 
-class FuncionDecl(Instruccion) :
+class FuncionDecl :
     '''
-        Esta clase representa declaracion de funcion.
-        La instrucción mientras recibe como parámetro una expresión lógica y la lista
-        de instrucciones a ejecutar si la expresión lógica es verdadera.
+        Esta clase representa la instruccion de declaracion de funcion.
+        Recibe como parámetro el identificador, lista de parametros (puede estar vacia) y
+        la lista de instrucciones.
     '''
 
-    def __init__(self, id, param = [], instrucciones = []) :
+    def __init__(self, fila, id, param = [], instrucciones = []) :
         self.id = id
+        self.fila = fila
         self.param = param
         self.instrucciones = instrucciones
 
-class Funcion(Instruccion) :
+class Funcion :
     '''
-        Esta clase representa declaracion de funcion.
-        La instrucción mientras recibe como parámetro una expresión lógica y la lista
-        de instrucciones a ejecutar si la expresión lógica es verdadera.
+        Esta clase representa la instruccion de llamado a funcion.
+        Recibe como parámetro el identificador y lista de parametros (puede estar vacia).
     '''
 
-    def __init__(self, id, param = []) :
+    def __init__(self, fila, id, param = []) :
         self.id = id
         self.param = param
+        self.fila = fila
 
-class Retorno(Instruccion) :
+class Retorno :
     '''
-        Esta clase representa declaracion de funcion.
-        La instrucción mientras recibe como parámetro una expresión lógica y la lista
-        de instrucciones a ejecutar si la expresión lógica es verdadera.
+        Esta clase representa la instrucción Retorno de una funcion.
+        Recibe como parámetro expresion a retornar.
     '''
 
-    def __init__(self,exp) :
+    def __init__(self, fila, exp) :
         self.exp = exp
+        self.fila = fila
+                
+class AgregarElemLista :
+    '''
+        Esta clase representa la instruccion de agregar elementos a una lista.
+        Recibe como parámetro el identificador de la lista y el elemento a añadir.
+    '''
+    def __init__(self, fila, id, exp) :
+        self.id=id
+        self.exp = exp
+        self.fila = fila
+        
+class Len :
+    '''
+        Esta clase representa la funcion len que calcula la longitud del parametro recibido
+        Recibe como parámetro una cadena o lista.
+    '''
+
+    def __init__(self,  cad) :
+        self.cad = cad
