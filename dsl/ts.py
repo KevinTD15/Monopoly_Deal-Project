888b33b2bde1a2ffc56c02e749495d50c8e9d607
KevinTD15 <talaveradiazkevin1999@gmail.com>
2023-01-06 07:13:27 -0500

    se arreglo eliminar de lista

--- dsl/ts.py
+++ dsl/ts.py
@@ -1,11 +1,10 @@
-from enum import Enum
-
 class Simbolo() :
     'Esta clase representa un simbolo dentro de nuestra tabla de simbolos'
 
-    def __init__(self, id, valor) :
+    def __init__(self, id, valor, tipo=0) :
         self.id = id
         self.valor = valor
+        self.tipo = tipo 
 
 class TablaDeSimbolos() :
     'Esta clase representa la tabla de simbolos (TS)'
@@ -18,20 +17,21 @@ class TablaDeSimbolos() :
     def chequeaIDenTS(self,id, filaError):
         'chequea si id existe en la TS, en ese caso muestra error e interrumpe la ejecucion'
         if not id in self.simbolos :
-            self.error(f'Variable \'{id}\' no definida', filaError)
+            if id=='jmpd':
+                self.error('El juego no ha sido creado',filaError)
+            else:
+                self.error(f'Variable \'{id}\' no definida', filaError)
 
     def obtener(self, id, filaError) :
         self.chequeaIDenTS(id, filaError)
         return self.simbolos[id]
 
-    def asignar(self, simbolo, filaError) :
-        if id in self.simbolos :
-            if type(self.simbolos.valor)!=type(simbolo.valor):
-                self.error(f'Variable \'{id}\' inconsistente' , filaError)   
+    def asignar(self, simbolo, tsFunciones, filaError) :
+        if simbolo.id in tsFunciones.simbolos :
+                self.error(f'\'{simbolo.id}\' definido como funcion y variable' , filaError)   
         self.simbolos[simbolo.id] = simbolo
 
     def eliminar(self, id, filaError): 
-        'Solo se utiliza para eliminar entrada en la TS de id=\'retorno\' una vez terminada ejecucion de la funcion (simulando pop de pila)'
         self.chequeaIDenTS(id, filaError)
         del self.simbolos[id]
 
@@ -44,7 +44,7 @@ class TablaDeSimbolos() :
 
     def eliminarElem(self, id, indice, filaError) :
         self.chequeaIDenTS(id, filaError)
-        self.simbolos[id].valor[indice]
+        self.simbolos[id].valor.pop(indice)
 
     def asignarElemList(self, id, indice,val, filaError) :
         self.chequeaIDenTS(id, filaError) 
@@ -59,21 +59,29 @@ class TablaDeSimbolos() :
         return self.simbolos[id].valor[indice]
     #endregion
     #region Tratamiento de las listas que estan definidas en el juego, no en el DSL
-    def agregarElemJuego(self, id, val) :
+    def agregarElemJuego(self, id, lista,  val) :
         'Agregar elemento a una lista definida en el juego'
-        id.append(val)
+        lista.append(val)
 
-    def obtenerElemListJuego(self, id, indice, filaError) :
+    def obtenerElemListJuego(self, id, lista, indice, filaError) :
         'Obtener elemento de una lista definida en el juego'
-        if indice>= len(id):
+        if indice>= len(lista):
             self.error(f'Lista \'{id}\' indice \'{indice}\' fuera de rango', filaError)          
-        return id[indice]
+        return lista[indice]
 
-    def asignarElemListJuego(self, id, indice,val, filaError) :
+    def asignarElemListJuego(self, id, lista, indice,val, filaError) :
         'Asignar un valor a una posicion de una lista definida en el juego'
-        if indice>= len(id):
+        if indice>= len(lista):
             self.error(f'Lista \'{id}\' indice \'{indice}\' fuera de rango', filaError)  
-        id[indice]=val
+        lista[indice]=val
+        
+    def eliminarElemListJuego(self, id, lista, indice, filaError) :
+        'Elemento de una lista definida en el juego'
+        if indice>= len(lista):
+            self.error(f'Lista \'{id}\' indice \'{indice}\' fuera de rango', filaError)          
+        return lista.pop(indice)
+        
+    
     #endregion            
     def error(self, cad, fila):
         print(f'mpd> Error en línea {fila}: {cad}')  
