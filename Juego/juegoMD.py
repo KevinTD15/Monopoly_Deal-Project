cc1ecedaa76145d1934cec8657a43a9de854cd4e
KevinTD15 <talaveradiazkevin1999@gmail.com>
2023-01-24 11:28:10 -0500

    modificado para annadir montecarlo

--- Juego/juegoMD.py
+++ Juego/juegoMD.py
@@ -4,6 +4,11 @@ from Jugador.jugadorMD import *
 import random as rd
 from Juego.crupier import *
 
+instanciaOriginal = None
+instanciaActual = None
+monteCarloActivo = False
+notificaciones = []
+turnosJugadorMC = 0
 class JuegoMD:
 
     _jugadores = []
@@ -13,61 +18,91 @@ class JuegoMD:
     descarte = []
     final = False
     ganador = 'Nadie'
-    notificaciones = []
+
     count = -1
                     
     def EjecutarTurnoMD(self):
         '''aqui es donde se lleva a cabo la ejecucion de un turno del juego'''
-        if(JuegoMD._iniciarJuegoMD):
-            JuegoMD._iniciarJuegoMD = False
-            Crupier.BarajarMazo(JuegoMD._mazo)
-            a = Crupier.RepartirCartas(True, JuegoMD._jugadores, JuegoMD._mazo, 5, JuegoMD.descarte)
+        global monteCarloActivo
+        if(self._iniciarJuegoMD):
+            self._iniciarJuegoMD = False
+            Crupier.BarajarMazo(self._mazo)
+            a = Crupier.RepartirCartas(True, self._jugadores, self._mazo, 5, self.descarte)
             if(a):
-                JuegoMD.final = True
-                JuegoMD.ganador = 'Nadie'        
-                JuegoMD.notificaciones.append('Se acabaron las cartas del mazo')
+                self.final = True
+                self.ganador = 'Nadie'        
+                if juego.monteCarloActivo==False:
+                    notificaciones.append('Se acabaron las cartas del mazo')
         else:
-            if(JuegoMD._indiceJugadorActual >= len(JuegoMD._jugadores)):
-                JuegoMD._indiceJugadorActual = 0
+            if(self._indiceJugadorActual >= len(self._jugadores)):
+                self._indiceJugadorActual = 0
             
-            jugadorActual = JuegoMD._jugadores[JuegoMD._indiceJugadorActual]
-            JuegoMD.notificaciones.append(f'Turno de: {jugadorActual.nombre}')
+            jugadorActual = self._jugadores[self._indiceJugadorActual]
+            if juego.monteCarloActivo==False:
+                notificaciones.append(f'Turno de: {jugadorActual.nombre}')
             
             if(len(jugadorActual.mano) == 0):
-                Crupier.RepartirCartas(False, jugadorActual, JuegoMD._mazo, 5, JuegoMD.descarte, JuegoMD._indiceJugadorActual)
-                JuegoMD.notificaciones.append(f'Se le repartieron 5 cartas a: {jugadorActual.nombre}')
+                Crupier.RepartirCartas(False, jugadorActual, self._mazo, 5, self.descarte, self._indiceJugadorActual)
+                if juego.monteCarloActivo==False:
+                    notificaciones.append(f'Se le repartieron 5 cartas a: {jugadorActual.nombre}')
             else:
-                Crupier.RepartirCartas(False, jugadorActual, JuegoMD._mazo, 2, JuegoMD.descarte, JuegoMD._indiceJugadorActual)
-                JuegoMD.notificaciones.append(f'{jugadorActual.nombre} toma 2 cartas del mazo')
+                Crupier.RepartirCartas(False, jugadorActual, self._mazo, 2, self.descarte, self._indiceJugadorActual)
+                if juego.monteCarloActivo==False:
+                    notificaciones.append(f'{jugadorActual.nombre} toma 2 cartas del mazo')
             
-            jugada = jugadorActual.SeleccionarJugada(JuegoMD._mazo, JuegoMD.descarte, JuegoMD._jugadores)
+            jugada = jugadorActual.SeleccionarJugada(self._mazo, self.descarte, self._jugadores)
             
-            jugadorActual.EjecutarJugada(jugada, JuegoMD._mazo, JuegoMD.descarte, JuegoMD._jugadores)
+            jugadorActual.EjecutarJugada(jugada, self._mazo, self.descarte, self._jugadores)
 
-            Crupier.VerificarMano(jugadorActual, JuegoMD._mazo, JuegoMD.descarte, JuegoMD._jugadores)
-            if(JuegoMD.final):
+            Crupier.VerificarMano(jugadorActual, self._mazo, self.descarte, self._jugadores)
+            if(self.final):
                 return
-            JuegoMD.final, JuegoMD.ganador = Crupier.FinDeJuegoMD(jugadorActual)
-            JuegoMD._indiceJugadorActual += 1
+            self.final, self.ganador = Crupier.FinDeJuegoMD(jugadorActual)
+            self._indiceJugadorActual += 1
                 
     def EjecutarJuego(self):
         '''aqui es donde se lleva a cabo la ejecucion del juego'''
         self.Reestablecer()
         while(not self.final):
-            JuegoMD.EjecutarTurnoMD(self)
-            JuegoMD.count += 1
+            self.ImprimirNotificaciones()
+            self.EjecutarTurnoMD()
+            self.count += 1
         
     def Reestablecer(self):
-        self = JuegoMD
-        JuegoMD.count = -1
-        JuegoMD._indiceJugadorActual = 0
-        JuegoMD.descarte = []
-        JuegoMD.notificaciones = []
-        JuegoMD.final = False
-        JuegoMD._iniciarJuegoMD = True
-        JuegoMD.ganador = 'Nadie'
-        JuegoMD._mazo = Mazo.cartas.copy()
+        self.count = -1
+        self._indiceJugadorActual = 0
+        self.descarte = []
+        self.final = False
+        self._iniciarJuegoMD = True
+        self.ganador = 'Nadie'
+        self._mazo = Mazo.cartas.copy()
         for i in self._jugadores:
             i.mano.clear()
             for k in i.tablero:
                 i.tablero[k].clear()
+                
+    def EjecutarSimulacion(self, dic, cp):
+        global instanciaOriginal
+        global instanciaActual
+        instanciaActual = None
+        total = 0
+        for i in range(cp):
+            notificaciones.append('COMIENZO DEL JUEGO')
+            if monteCarloActivo==False:
+                instanciaOriginal=self
+            self.EjecutarJuego()       
+            dic[self.ganador] += 1
+            total += 1
+            #notificaciones.clear()  
+            notificaciones.append(f'Gano: {self.ganador} en {self.count} turnos')
+            notificaciones.append(f'*************************           FIN DEL JUEGO {i+1}')
+            for i in notificaciones:
+                print(i)
+            notificaciones.clear()
+        self.ImprimirNotificaciones()
+        return dic
+
+    def ImprimirNotificaciones(self):
+        for i in notificaciones:
+            print(i)
+        notificaciones.clear()
